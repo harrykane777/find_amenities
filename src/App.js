1f25b7e103063414edbae88b8b5dc77f0f0fddd9
Nelson Lin <nelson.lin2308@gmail.com>
2024-01-29 15:51:09 +1100

    Removed search page component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import SearchPage from './components/SearchPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/search" element={<SearchPage />} />
       </Routes>
     </BrowserRouter>
   );
