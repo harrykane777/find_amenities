46429126172ebf5c38265402465cdfc1f590e769
issac <issac.arjs@gmail.com>
2024-01-29 16:42:13 +1100

    initial available spaces

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import AvailableSpaces from './AvailableSpaces';
 
 const LandingPage = () => {
   return (
     <div>
+      <AvailableSpaces />
       Landing Page
     </div>
   );
