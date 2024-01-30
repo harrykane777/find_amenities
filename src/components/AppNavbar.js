2c5567cbacbcd99444a7d1a067afc26f3de264eb
Nelson Lin <nelson.lin2308@gmail.com>
2024-01-30 13:07:23 +1100

    navbar

--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -7,15 +7,17 @@ const AppNavbar = () => {
     return (
         <Navbar className="p-2" style={{ backgroundColor: "#6f42c1" }}>
             <Navbar.Brand className="fs-3" style={{ color: "white" }} href="/">Car Park Finder</Navbar.Brand>
-            <Navbar.Collapse className="justify-content-center">
-                <Nav>
+            <Navbar.Collapse className="justify-content-center" style={{ margin: "0 15%" }}>
+                <Nav className="flex-grow-1 justify-content-end">
                     <Nav.Item>
-                        <Nav.Link href="/browse" className="fs-5 mx-5" style={{ color: "white" }}>Browse</Nav.Link>
+                        <Nav.Link href="/browse" className="fs-5" style={{ color: "white", marginRight: "20px" }}>Browse</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item className="flex-grow-1 align-self-center">
+                        <Form>
+                            <Form.Control placeholder="Search" />
+                        </Form>
                     </Nav.Item>
                 </Nav>
-                <Form>
-                    <Form.Control placeholder="Search" className="flex-grow-1" />
-                </Form>
             </Navbar.Collapse>
         </Navbar>
     );
