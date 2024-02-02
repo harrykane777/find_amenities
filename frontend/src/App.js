941e32eead55b389499d3717743f6ff052803522
issac <issac.arjs@gmail.com>
2024-02-02 21:41:20 +1100

    backend/middleware setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import ViewCarParkPage from './pages/ViewCarParkPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/viewCarPark" element={<ViewCarParkPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
